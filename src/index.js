c2f05ff0dbca09e427e138df5086577965b8fe38
zach <zachbogard@gmail.com>
2019-07-13 12:41:35 -0700

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as dat from 'dat-gui'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const gui = new dat.GUI()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let resizeCanvas = function () {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    c.imageSmoothingEnabled = false;
+}
+
+window.addEventListener('resize', function () {
+    resizeCanvas()
+})
+
+// window.onresize
+
+// const props = {
+//     start: canvas.height * 0.8,
+//     length: 0.0007,
+//     amp: 40,
+//     duration: 0.015,
+//     magnitude: 1.15
+// }
+
+// const props = {
+//     start: canvas.height * 0.8,
+//     length: 0.0007,
+//     amp: 60,
+//     duration: 0.007,
+//     magnitude: 1.2
+// }
+
+const props = {
+    start: canvas.height * 0.6,
+    length: 0.0003,
+    amp: 0,
+    duration: 0.003,
+    magnitude: 1
+}
+
+gui.add(props, 'start', 0, canvas.height)
+gui.add(props, 'length', -.03, .03)
+gui.add(props, 'amp', 0, 100)
+gui.add(props, 'duration', -.01, .1)
+gui.add(props, 'magnitude', 1, 2)
+
+// let direction = true
+let increment = props.duration
+
+
+function animate() {
+
+    // if (change % 20 === 0) {
+    //     directionChange()
+    // }
+    requestAnimationFrame(animate)
+
+
+    //Top
+    c.fillStyle = "rgba(70, 130, 180,0.9)"
+
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    // Bottom
+    c.beginPath()
+
+    c.moveTo(0, props.start)
+
+
+    for (let i = 0; i <= canvas.width; i++) {
+        c.lineTo(i, props.start + 40 * Math.sin(i * 0.01 * Math.sin(increment)) + Math.sin(i ** (props.magnitude) * (props.length + 5 * Math.sin(increment) / 10000)) * props.amp ** ((i + .1 * canvas.width) / canvas.width * 1.1))
+    }
+
+    c.lineTo(canvas.width, canvas.height)
+    c.lineTo(0, canvas.height)
+    c.fillStyle = "ghostwhite"
+    c.fill()
+
+    increment -= props.duration
+}
+animate()
