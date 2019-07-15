d880bc5ff1989592d06b1609e35acd346d110909
zach <zachbogard@gmail.com>
2019-07-14 19:56:34 -0700

    Added logo

--- src/js/SmoothScroll.js
+++ src/js/SmoothScroll.js
@@ -2,28 +2,30 @@ import $ from "jquery"
 
 // Smooth Scroll
 export default function SmoothScroll() {
+    $(document).ready(() => {
+        let scrollLink = $('.scroll')
 
-    let scrollLink = $('.scroll')
+        // Smooth Scrolling
+        scrollLink.click(function (e) {
+            e.preventDefault()
+            $('body,html').animate({
+                scrollTop: $(this.hash).offset().top
+            })
+        })
+        // Active Link Switching
+        $(window).scroll(function () {
+            let scrollbarLocation = $(this).scrollTop()
+            console.log(scrollbarLocation)
 
-    // Smooth Scrolling
-    scrollLink.click(function (e) {
-        e.preventDefault()
-        $('body,html').animate({
-            scrollTop: $(this.hash).offset().top
+            scrollLink.each(function () {
+                let sectionOffset = $(this.hash).offset().top - 20
+
+                if (sectionOffset <= scrollbarLocation) {
+                    $(this).addClass('active');
+                    $(this).parent().siblings().children().removeClass('active')
+                }
+            })
         })
     })
-    // Active Link Switching
-    $(window).scroll(function () {
-        let scrollbarLocation = $(this).scrollTop()
-        console.log(scrollbarLocation)
 
-        scrollLink.each(function () {
-            let sectionOffset = $(this.hash).offset().top - 20
-
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).addClass('active');
-                $(this).parent().siblings().children().removeClass('active')
-            }
-        })
-    })
 }
